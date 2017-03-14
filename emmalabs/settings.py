fc0159c6559643fa8232be45f38a41cd52952b73
juan <juancamilogaviriaacosta@gmail.com>
2017-03-13 22:47:03 -0500

    commit juan 2017-03-13_22:47

--- emmalabs/settings.py
+++ emmalabs/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'emmalabs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'emmalabs',
+        'USER': 'core',
+        'PASSWORD': 'core',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
