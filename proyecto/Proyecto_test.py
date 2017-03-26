ab7c02ce0c60aae189599caec3292ff969b8e343
DESKTOP-NLPPM1R\usuario <warrdnez@hotmail.com>
2017-03-26 16:20:52 -0500

    adding test for the projects issue

---
+++ proyecto/Proyecto_test.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from unittest import TestCase
+from selenium import webdriver
+
+class ProyectoTest(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000/admin')
+        self.assertIn('Busco Ayuda', self.browser.title)
