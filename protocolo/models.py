0c98f82db9c61d0c80f7bd98493d6977a600789d
mpuertog <ms.puerto@uniandes.edu.co>
2017-03-26 23:03:09 -0500

    First version of the protocolo search form

--- protocolo/models.py
+++ protocolo/models.py
@@ -6,23 +6,15 @@ from django.db import models
 from insumo.models import Insumo
 
 
-class Paso(models.Model):
-    nombre = models.CharField(max_length=50, null=False)
-    entradas = models.ManyToManyField(Insumo, related_name="entradas")
-    salidas = models.ManyToManyField(Insumo, related_name="salidas")
-
-
 class ClasificacionProtocolo(models.Model):
     nombre_clasificacion = models.CharField(max_length=50, default="Sin clasificación")
 
 
 class Protocolo(models.Model):
     nombre = models.CharField(max_length=50, blank=True, null=True)
-    descripcion = models.CharField(max_length=50, blank=True, null=True)
+    descripcion = models.CharField(max_length=5000, blank=True, null=False)
     version = models.FloatField(default=1.0)
-    pasos = models.ManyToManyField(Paso, related_name="pasos")
+    insumos = models.ManyToManyField(Insumo, related_name="insumos")
     fecha_creacion = models.DateField(default=datetime.now, blank=True, null=False)
-    codigo = models.CharField(max_length=10, default="PRO-000", blank=True , null=False)
+    codigo = models.CharField(max_length=10, default="PRO-000", blank=True, null=False)
     clasificacion = models.ForeignKey(ClasificacionProtocolo, related_name="clasificacion")
-
-
