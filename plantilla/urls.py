a8edf3851721dd5b2994331cff4213c422be046c
juan <juancamilogaviriaacosta@gmail.com>
2017-04-06 12:16:22 -0500

    Plantilla maestra

--- plantilla/urls.py
+++ plantilla/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+    url(r'^$', views.index),
+    url(r'^index.html/$', views.index),
+    url(r'^about.html/$', views.about),
+    url(r'^contact.html/$', views.contact),
+    url(r'^post.html/$', views.post)
 ]
--- plantilla/views.py
+++ plantilla/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def post(request):
+    return render(request, 'post.html')
