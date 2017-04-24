7addf8e921f1fc26c8e76c2c7b3c4f70babdbb22
juan <juancamilogaviriaacosta@gmail.com>
2017-04-24 01:03:51 -0500

    Modelo resultado

---
+++ resultado/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from experimento.models import Experimento
+from protocolo.models import Protocolo
+
+class Resultado(models.Model):
+    resultado = models.CharField(max_length=50, blank=True, null=True)
+    satisfactorio = models.CharField(max_length=50, blank=True, null=True)
+    observaciones = models.CharField(max_length=50, blank=True, null=True)
+    fecha_resultado = models.DateTimeField(blank=True, null=True)
+    experimento = models.ForeignKey(Experimento, related_name="experimento")
+    protocolo = models.ForeignKey(Protocolo, related_name="protocolo")
