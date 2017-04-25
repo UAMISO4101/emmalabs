19ab6a9c8562d60f3d82a5583d8407d0e70810f7
juan <juancamilogaviriaacosta@gmail.com>
2017-04-24 22:33:51 -0500

    Guardar formulario resultado

--- resultado/views.py
+++ resultado/views.py
@@ -1,16 +1,33 @@
 # coding=utf-8
 from django.shortcuts import render
+from django.contrib import messages
 from experimento.models import Experimento
 from proyecto.models import Proyecto
 from protocolo.models import Protocolo
+from resultado.models import Resultado
 
 def registrarResultado(request, id):
     if(request.method == "POST"):
-        resultado = request.POST['resultado']
-        id = request.POST['experimento_id']
-        experimento = Experimento.objects.get(id=id)
-        experimento.resultado = resultado
-        experimento.save()
+
+        resultados = request.POST['resultados']
+        satisfactorio = request.POST['satisfactorio']
+        obrevaciones = request.POST['obrevaciones']
+        fecha = request.POST['fecha']
+        proyecto = request.POST['proyecto']
+        experimento = request.POST['experimento']
+        protocolo = request.POST['protocolo']
+
+        resultado = Resultado()
+        resultado.resultado = resultados
+        resultado.satisfactorio = satisfactorio
+        resultado.observaciones = obrevaciones
+        resultado.fecha_resultado = fecha
+        resultado.proyecto = Proyecto.objects.get(id=proyecto)
+        resultado.experimento = Experimento.objects.get(id=experimento)
+        resultado.protocolo = Protocolo.objects.get(id=protocolo)
+        resultado.save()
+        messages.success(request, "Resultado guardado", extra_tags="alert-success")
+
     experimento = Experimento.objects.get(id=id)
     proyectos = Proyecto.objects.all()
     protocolos = Protocolo.objects.all()
