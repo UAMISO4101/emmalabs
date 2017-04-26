57580435d3d5a04214fe253ac6ec795c45fd84ce
mpuertog <ms.puerto@uniandes.edu.co>
2017-04-26 12:30:24 -0500

    Added user reference to fix bug HU107

--- proyecto/views.py
+++ proyecto/views.py
@@ -1,8 +1,19 @@
 # coding=utf-8
 from django.shortcuts import render
+from usuario.models import Usuario
 from .models import Proyecto
 
+
 def proyectos(request):
-    lista_proyectos = Proyecto.objects.all()
+    # Inicializar variables
+    usuario_actual = request.user
+    lista_proyectos = []
+
+    if request.user.is_authenticated:
+        print("Autenticado!")
+        # Cargar los proyectos solo si el usuario ha iniciado sesión
+        lista_proyectos = Proyecto.objects.filter(asistente__asistente__cientifico_id=usuario_actual.id)
+
+
     context = {'lista_proyectos': lista_proyectos}
     return render(request, 'proyectos.html', context)
