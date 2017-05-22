9bb5751616ae6b6d2dce18a6a43279f5fbbb0a53
juan <juancamilogaviriaacosta@gmail.com>
2017-05-21 19:05:06 -0500

    Modelo comentarios

--- protocolo/admin.py
+++ protocolo/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from protocolo.models import Protocolo, ClasificacionProtocolo, Paso
+from protocolo.models import Protocolo, ClasificacionProtocolo, Paso, ComentarioProtocolo
 
 admin.site.register(Protocolo)
 admin.site.register(ClasificacionProtocolo)
 admin.site.register(Paso)
+admin.site.register(ComentarioProtocolo)
--- protocolo/models.py
+++ protocolo/models.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-
 from django.db import models
-
 from insumo.models import Insumo
-
+from usuario.models import Usuario
 
 class ClasificacionProtocolo(models.Model):
     nombre_clasificacion = models.CharField(max_length=50, default="Sin clasificacion")
-
 
 class Protocolo(models.Model):
     nombre = models.CharField(max_length=50, blank=True, null=True)
@@ -20,7 +17,12 @@ class Protocolo(models.Model):
     clasificacion = models.ForeignKey(ClasificacionProtocolo, related_name="clasificacion")
     observaciones = models.CharField(max_length=500, blank=True, null=False)
 
-
 class Paso(models.Model):
     descripcion = models.CharField(max_length=5000, blank=False, null=False)
     protocolo = models.ForeignKey(Protocolo, on_delete=models.CASCADE)
+
+class ComentarioProtocolo(models.Model):
+    texto = models.CharField(max_length=5000, blank=False, null=False)
+    fecha_creacion = models.DateField(default=datetime.now, blank=True, null=False)
+    protocolo = models.ForeignKey(Protocolo)
+    usuario = models.ForeignKey(Usuario)
