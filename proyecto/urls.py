9db7ec86c1a6de14d158eda9a74c213be43dd923
Cristian Olaya <cristianolaya@MacBook-Pro-de-Cristian.local>
2017-05-21 22:25:57 -0500

    creando template de proyectos

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^proyectos/$', views.proyectos),
+    url(r'^crearProyecto/$', views.crear_proyecto, name='crearProyecto'),
 ]
--- proyecto/views.py
+++ proyecto/views.py
@@ -1,19 +1,48 @@
 # coding=utf-8
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from proyecto.forms import ProyectoForm
 from usuario.models import Usuario
 from .models import Proyecto
+from django.urls import reverse
 
 
 def proyectos(request):
     # Inicializar variables
-    usuario_actual = request.user
+    usuario_actual = Usuario.objects.get(user=request.user)
     usuario_parametro = Usuario.objects.get(user_id=usuario_actual.id)
     lista_proyectos = []
 
-    if usuario_actual.is_authenticated:
-        # Cargar los proyectos del usuario ha iniciado sesión
+    # if usuario_actual.is_authenticated:
+    # Cargar los proyectos del usuario ha iniciado sesión
+    if usuario_actual.rol_usuario == "rol_asistente":
         lista_proyectos = Proyecto.objects.filter(asistentes__user__usuario=usuario_parametro.id)
+    else:
+        lista_proyectos = Proyecto.objects.all()
 
     context = {'lista_proyectos': lista_proyectos}
-    
+
     return render(request, 'proyectos.html', context)
+
+def crear_proyecto(request):
+    if request.method == 'POST':
+        form = ProyectoForm(request.POST)
+        # Validar formulario
+        if form.is_valid():
+            proyecto = form.save(commit=False)
+            # Se debe guardar el usuario creador
+            proyecto.usuario_creador = Usuario.objects.get(user=request.user)
+            # Guardar la solicitud
+            proyecto.save()
+            # Cargar mensaje de exito
+            messages.add_message(request, messages.SUCCESS, 'El proyecto se ha creado correctamente')
+            # Retornar a la pagina crearSolicitud
+            return HttpResponseRedirect(reverse('crearProyecto'))
+        else:
+            # Visualizar errores presentados
+            print(form.errors)
+    else:
+        form = ProyectoForm()
+
+    return render(request, 'crearProyecto.html', {'form':form})
