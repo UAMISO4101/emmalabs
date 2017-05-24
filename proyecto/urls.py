f589f1cd88f3174886c4d950c5eab0ac86ba50ce
mpuertog <ms.puerto@uniandes.edu.co>
2017-05-24 09:15:26 -0500

    Fixed URL redirections

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^proyectos/$', views.proyectos),
+    url(r'^proyectos/$', views.proyectos, name='proyectos'),
     url(r'^crearProyecto/$', views.crear_proyecto, name='crearProyecto'),
 ]
